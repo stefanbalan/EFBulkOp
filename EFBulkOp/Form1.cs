bd1026e08344d850aaf338d8382a94762765d409
Stefan Balan <stefan.balan@gmail.com>
2018-01-18 13:35:41 +0200

    run 100k test

--- EFBulkOp/Form1.cs
+++ EFBulkOp/Form1.cs
@@ -17,11 +17,11 @@ namespace EFBulkOp
 
         private void btnRunAll_Click(object sender, EventArgs e)
         {
-            new Test1().Run(10_000);
-            new Test2().Run(10_000);
-            new Test3().Run(10_000);
-            new Test4().Run(10_000);
-            new Test5().Run(10_000);
+            //new Test2().Run(10_000); // removed, are too slow for testing with more than 10k 
+            //new Test3().Run(10_000); // removed, are too slow for testing with more than 10k
+            //new Test4().Run(10_000); // removed, are too slow for testing with more than 10k
+            new Test1().Run(100_000);
+            new Test5().Run(100_000);
         }
 
         private void button2_Click(object sender, EventArgs e)
